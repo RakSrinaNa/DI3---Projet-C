684cd9dd8822482ff705d6eda7605a99de652ac8
Victor Coleau <21402028t@MASTERLINUX>
2016-12-06 16:00:37 +0100

    trucs

--- parser.c
+++ parser.c
@@ -1,3 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void readFile (char * fileName){
+
+    FILE * file = fopen(fileName);
+    Instance instance;
+
+    int instanceNumber = atoi(readLine(file)); // Read number of instances
+
+    char * line = readLine(file); // Read first line
+
+    instance.objectNumber = atoi(stringToTab(line)[0]); // Set number of objects
+    instance.dimensionNumber = atoi(stringToTab(line)[1]); // Set number of dimensions
+
+    readLine(file); // Read empty line
+
+
+    fclose(file);
+
+}
+
+int * stringToTab(char * string){
+
+    int * tab = malloc(sizeof(int));
+    int number = 0;
+
+    while (*string != '\0'){
+
+
+    }
+
+}
