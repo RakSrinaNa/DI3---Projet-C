74555a47469fe42854c86dd4981d03bee311c24b
Thomas Couchoud <zerderr@gmail.com>
2016-12-06 15:12:57 +0100

    Add cmake

---
+++ cmake-build-debug/CMakeFiles/Projet2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet2.dir/main.c.o"
+  "Projet2.pdb"
+  "Projet2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
