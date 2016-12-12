7567e9e331e66c29f2cfd0a05faf48fe08010df7
Thomas Couchoud <zerderr@gmail.com>
2016-12-12 14:00:51 +0100

    Clean code

--- src/solutionIndirect.c
+++ src/solutionIndirect.c
@@ -6,7 +6,6 @@
 
 SolutionIndirect * solutionIndirect_create(Instance * instance)
 {
-	
 	SolutionIndirect * solution;
 	if((solution = (SolutionIndirect *) malloc(sizeof(SolutionIndirect))) == NULL)
 	{
@@ -41,7 +40,6 @@ void solutionIndirect_destroy(SolutionIndirect * solution)
 
 void solutionIndirect_decode(Instance * instance, SolutionIndirect * solution)
 {
-	
 	Bag * bag = bag_create(instance);
 	
 	for(int i = 0; i < instance->itemsCount; i++)
@@ -64,7 +62,6 @@ int solutionIndirect_evaluate(Instance * instance, Bag * bag)
 
 int solutionIndirect_doable(Instance * instance, Bag * bag)
 {
-	
 	for(int i = 0; i < instance->dimensionsNumber; i++)
 		if(bag->weights[i] > instance->maxWeights[i])
 			return 0;
@@ -74,7 +71,6 @@ int solutionIndirect_doable(Instance * instance, Bag * bag)
 
 void solutionIndirect_print(Instance * instance, Bag * bag)
 {
-	
 	printf("Total value in the bag : %d\n", solutionIndirect_evaluate(instance, bag));
 	printf("Objects in the bag : ");
 	for(unsigned int i = 0; i < bag->itemsCount; i++)
@@ -84,7 +80,6 @@ void solutionIndirect_print(Instance * instance, Bag * bag)
 
 void solutionIndirect_saveToFile(char * fileName, Instance * instance, SolutionIndirect * solution, Bag * bag)
 {
-	
 	FILE * file;
 	if((file = fopen(fileName, "w+")) == NULL)
 	{
