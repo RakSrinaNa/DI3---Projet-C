71d2753c293192e0a30d6b16b4e39c6f446be0ff
NoahReyson <shadow.reyson@gmail.com>
2016-12-15 11:41:09 +0100

    new scheduler

--- src/scheduler.c
+++ src/scheduler.c
@@ -15,7 +15,7 @@ int * scheduler_itemValue(Instance * instance)
 
     for(int i = 0; i < instance->itemsCount-1; i++){
         for (int j = 0; j < instance->itemsCount-1-i; j++){
-            if (instance_item_getValue(instance, list[j]) > instance_item_getValue(instance, list[j+1])){
+            if (instance_item_getValue(instance, list[j]) < instance_item_getValue(instance, list[j+1])){
                 int t = list[j];
                 list[j] = list[j+1];
                 list[j+1] = t;
@@ -24,4 +24,33 @@ int * scheduler_itemValue(Instance * instance)
     }
 
     return list;
+}
+
+int * scheduler_ratioAllDimensions(Instance * instance)
+{
+    int * list = (int *)malloc(sizeof(int) * instance->itemsCount);
+
+    for(int i = 0; i < instance->itemsCount; i++)
+        list[i] = i;
+
+    for(int i = 0; i < instance->itemsCount-1; i++){
+        for (int j = 0; j < instance->itemsCount-1-i; j++){
+            if (scheduler_getRatioAllDimensions(instance_getItem(instance, list[j]), instance->dimensionsNumber) < scheduler_getRatioAllDimensions(instance_getItem(instance, list[j+1]), instance->dimensionsNumber)){
+                int t = list[j];
+                list[j] = list[j+1];
+                list[j+1] = t;
+            }
+        }
+    }
+
+    return list;
+}
+
+double scheduler_getRatioAllDimensions(Item * item, int DCount)
+{
+    double totalWeight;
+    for(int i = 0; i < DCount; i++)
+        totalWeight += item->weights[i];
+
+    return (item->value)/totalWeight;
 }
