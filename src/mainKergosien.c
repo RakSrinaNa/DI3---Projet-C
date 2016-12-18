46f25a3a8161e87ed58299d703376a3ced2e3b12
Thomas Couchoud <zerderr@gmail.com>
2016-12-18 11:17:26 +0100

    Fix compilation error

--- src/mainKergosien.c
+++ src/mainKergosien.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <memory.h>
 
 #include "instance.h"
 #include "parser.h"
@@ -10,10 +11,9 @@
 
 void mainKergosien()
 {
-    Instance * instances = parser_readFile("F:\\Polytech\\ProjetTutore\\DI3---Projet2\\MKP-Instances");
+    Instance * instances = parser_readFile("./MKP-Instances/_mknapcb1_res.txt");
 
     for(int i = 0; i < 30; i++)
-    {
         for(int j = 0; j < 4; j++)
         {
             Solution * solution = heuristic(&(instances[i]), 1, j);
@@ -23,7 +23,6 @@ void mainKergosien()
             heuristic_solutionDestroy(solution);
             instance_destroy(&(instances[i]));
         }
-    }
 
     free(instances);
 
@@ -34,9 +33,9 @@ char * stringConcat(char * s1, char * s2)
     char * string = (char *)malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
     int i = 0;
     int j = 0;
-    for(i = 0; i < strlen(s1); i++)
+    for(i = 0; i < (int) strlen(s1); i++)
         string[i+j] = s1[i];
-    for(j = 0; j < strlen(s2); j++)
+    for(j = 0; j < (int) strlen(s2); j++)
         string[i+j] = s2[j];
     string[i+j] = '\0';
 
@@ -50,7 +49,7 @@ char * stringConcat(char * s1, char * s2)
 char * stringCopy(char * string)
 {
     char * str = (char *)malloc(strlen(string) * sizeof(char));
-    for(int i = 0; i < strlen(string); i++)
+    for(int i = 0; i < (int) strlen(string); i++)
         str[i] = string[i];
 
     return str;
