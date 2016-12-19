4fc17fa525c0f7554eb5d97973475e8ad4c17186
Thomas Couchoud <zerderr@gmail.com>
2016-12-19 16:35:48 +0100

    Add scheduler doc

--- src/scheduler.h
+++ src/scheduler.h
@@ -3,6 +3,12 @@
 
 #include "instance.h"
 
+/**
+ * Creates a list of items indexes of an instance. This list is randomly ordered.
+ *
+ * @param instance A pointer to the instance containing the items.
+ * @return The list of items.
+ */
 int * scheduler_random(Instance * instance);
 
 /**
@@ -15,20 +21,73 @@ int * scheduler_random(Instance * instance);
  */
 int scheduler_removeFromList(int ** listPtr, int * listCount, int index);
 
+/**
+ * Append an item to the end of the list.
+ *
+ * @param list A pointer to the list to insert the item into.
+ * @param listCount A pointer to the list length.
+ * @param element The element to insert.
+ */
 void scheduler_appendToList(int ** list, int * listCount, int element);
 
+/**
+ * Creates a list of items indexes of an instance. This list is ordered by the item value (decreasing).
+ *
+ * @param instance A pointer to the instance containing the items.
+ * @return The list of items.
+ */
 int * scheduler_itemValue(Instance * instance);
 
+/**
+ * Creates a list of items indexes of an instance. This list is ordered by the ratio of the value and the sum of its weights (decreasing).
+ *
+ * @param instance A pointer to the instance containing the items.
+ * @return The list of items.
+ */
 int * scheduler_ratioAllDimensions(Instance * instance);
 
+/**
+ * Get the ratio of an item over all the dimensions.
+ *
+ * @param instance The instance where the item is in.
+ * @param index The index of the item.
+ * @return Its ratio (value/(sum of weights)).
+ */
 double scheduler_getRatioAllDimensions(Instance * instance, int index);
 
+/**
+ * Creates a list of items indexes of an instance. This list is ordered by the ratio of the value and the specified weight (decreasing).
+ *
+ * @param instance A pointer to the instance containing the items.
+ * @return The list of items.
+ */
 int * scheduler_ratioForDimension(Instance * instance, int dimension, int * itemsInList, int sizeList);
 
+/**
+ * Get the ratio of an item over the specified dimension.
+ *
+ * @param instance The instance where the item is in.
+ * @param index The index of the item.
+ * @param dim The index of the dimension.
+ * @return Its ratio.
+ */
 double scheduler_getRatio(Instance * instance, int index, int dim);
 
+/**
+ * Creates a list of items indexes of an instance. This list is ordered by the ratio of the value and the sum of its weighted weights (decreasing).
+ *
+ * @param instance A pointer to the instance containing the items.
+ * @return The list of items.
+ */
 int * scheduler_ratioAllDimensionsWeighted(Instance * instance);
 
+/**
+ * Get the ratio of an item over all the weighted dimensions.
+ *
+ * @param instance The instance where the item is in.
+ * @param index The index of the item.
+ * @return Its ratio (value/(sum of (maxWeight - weight))).
+ */
 double scheduler_getRatioAllDimensionsWeighted(Instance * instance, int index);
 
 #endif
