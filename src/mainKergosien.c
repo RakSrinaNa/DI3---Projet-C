560a3ba60df542d306e0d1ff925b2d903742c7d7
NoahReyson <shadow.reyson@gmail.com>
2016-12-21 03:07:27 +0100

    PAREIL

--- src/mainKergosien.c
+++ src/mainKergosien.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <memory.h>
+#include <dirent.h>
 
 #include "instance.h"
 #include "parser.h"
@@ -7,47 +8,44 @@
 
 void mainKergosien()
 {
-	Parser * parser = parser_create("./MKP-Instances/_mknapcb1_res.txt");
-	
-	Instance * instance;
-	while((instance = parser_getNextInstance(parser)) != NULL)
-	{
-		for(int j = 0; j < 4; j++)
-		{
-			Solution * solution = heuristic(instance, 1, j);
-			char str[50];
-			sprintf(str, "test1_direct_scheduler_%d.txt", j);
-			heuristic_saveSolutionToFile(str, instance, solution);
-			heuristic_solutionDestroy(solution);
-		}
-		instance_destroy(instance);
-	}
-	
-	parser_destroy(parser);
-}
+    char dirName[] = "./MKP-Instances";
+    char str[200];
+    DIR * dir = opendir(dirName);
 
-char * stringConcat(char * s1, char * s2)
-{
-	char * string = (char *) malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
-	int i = 0;
-	int j = 0;
-	for(i = 0; i < (int) strlen(s1); i++)
-		string[i + j] = s1[i];
-	for(j = 0; j < (int) strlen(s2); j++)
-		string[i + j] = s2[j];
-	string[i + j] = '\0';
-	
-	free(s1);
-	free(s2);
-	
-	return string;
-}
+    int i = 0;
 
-char * stringCopy(char * string)
-{
-	char * str = (char *) malloc(strlen(string) * sizeof(char));
-	for(int i = 0; i < (int) strlen(string); i++)
-		str[i] = string[i];
-	
-	return str;
+    struct dirent * file;
+    while((file = readdir(dir)) != NULL)
+    {
+        if(strcoll(file->d_name, "mknap") == NULL)
+            continue;
+
+        i++;
+
+        sprintf(str, "%s/%s", dirName, file->d_name);
+        Parser * parser = parser_create(str);
+
+        Instance * instance;
+        while((instance = parser_getNextInstance(parser)) != NULL)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                char str2[50];
+                Solution * solution = heuristic(instance, 1, j);
+                sprintf(str2, "Solutions/test1_direct_file_%d_scheduler_%d.txt", i, j);
+                heuristic_saveSolutionToFile(str2, instance, solution);
+                heuristic_solutionDestroy(solution);
+
+                solution = heuristic(instance, 0, j);
+                sprintf(str2, "Solutions/test1_indirect_file_%d_scheduler_%d.txt", i, j);
+                heuristic_saveSolutionToFile(str2, instance, solution);
+                heuristic_solutionDestroy(solution);
+            }
+            instance_destroy(instance);
+        }
+        parser_destroy(parser);
+
+    }
+
+    closedir(dir);
 }
