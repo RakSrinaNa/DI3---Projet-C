d6565f0a16fc8cfbd471407938c200aaf9a6811b
Thomas Couchoud <zerderr@gmail.com>
2017-01-11 08:28:50 +0100

    Handle temp argv

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include "mainUnit.h"
 #include "mainKergosien.h"
 #include "parser.h"
+#include "solution.h"
+#include "heuristic.h"
 
 int main(int argc, char * argv[])
 {
@@ -22,6 +24,16 @@ int main(int argc, char * argv[])
 		else if(strcmp(argv[1], "ker") == 0)
 		{
 			mainKergosien();
+		}
+		else if(strcmp(argv[1], "temp") == 0)
+		{
+			Parser * parser = parser_create("MKP-Instances/theBestBag2.txt");
+			Instance * instance = parser_getNextInstance(parser);
+			Solution * solution = heuristic(instance, DIRECT, 5);
+			solutionDirect_print(solution->solutions.direct);
+			solution_destroy(solution);
+			instance_destroy(instance);
+			parser_destroy(parser);
 		}
 	}
 	else
