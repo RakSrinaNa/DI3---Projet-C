9872b36d6160f25a23b9b98ac96ef3b37191c125
Thomas Couchoud <zerderr@gmail.com>
2017-01-12 12:33:20 +0100

    Add test

--- src/metaheuristicTabouUnit.c
+++ src/metaheuristicTabouUnit.c
@@ -4,6 +4,9 @@
 #include "utils.h"
 #include "metaheuristicTabou.h"
 #include "unit.h"
+#include "parser.h"
+#include "solution.h"
+#include "instance.h"
 
 void metaheuristicTabouTests()
 {
@@ -25,5 +28,30 @@ void movementTests()
 	if(movement_equals(m1, m2) != 1)
 		unit_error("ASSERT MOVEMENT 2");
 	free(m1);
+	
+	int correctOrder[3] = {2, 1, 0};
+	int * itemOrder;
+	MMALLOC(itemOrder, int, 3, NULL);
+	itemOrder[0] = 0;
+	itemOrder[1] = 1;
+	itemOrder[2] = 2;
+	Instance * instance = parser_readAllFile("MKP-Instances/theBestBag2.txt");
+	Solution * solution;
+	MMALLOC(solution, Solution, 1, NULL);
+	solution->instance = instance;
+	solution->type = INDIRECT;
+	solution->solveTime = 0;
+	solution->solutions.indirect = solutionIndirect_create(instance);
+	solution->solutions.indirect->itemsOrder = itemOrder;
+	m2->a = 0;
+	m2->b = 2;
+	
+	movement_applyMovement(solution, m2);
+	
+	if(!unit_arrayEquals(correctOrder, solution->solutions.indirect->itemsOrder, instance->itemsCount))
+		unit_error("ASSERT MOVEMENT 3");
+	
+	solution_destroy(solution);
+	instance_destroy(instance);
 	free(m2);
 }
